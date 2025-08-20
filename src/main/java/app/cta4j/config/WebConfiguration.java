cfd26f88732fe1687d3a9422b194ad456cc8b78a
Logan Kulinski <lbkulinski@gmail.com>
2025-08-20 01:41:39 -0500

    Cors fixes

--- src/main/java/app/cta4j/config/WebConfiguration.java
+++ src/main/java/app/cta4j/config/WebConfiguration.java
@@ -1,6 +1,7 @@
 package app.cta4j.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,7 +26,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         Objects.requireNonNull(registry);
 
         registry.addMapping("/**")
