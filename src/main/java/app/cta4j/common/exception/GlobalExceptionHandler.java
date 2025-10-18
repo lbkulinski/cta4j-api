ecb135ff4405ecde5680f0b4678cc887c5ef1e24
Logan Kulinski <lbkulinski@gmail.com>
2025-10-18 14:03:05 -0500

    Added exception handler

---
+++ src/main/java/app/cta4j/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package app.cta4j.common.exception;
+
+import com.cta4j.exception.Cta4jException;
+import com.rollbar.notifier.Rollbar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public final class GlobalExceptionHandler {
+    private final Rollbar rollbar;
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @Autowired
+    public GlobalExceptionHandler(Rollbar rollbar) {
+        this.rollbar = rollbar;
+    }
+
+    @ExceptionHandler(Cta4jException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public void handleCta4jException(Cta4jException exception) {
+        String message = "A cta4j exception occurred";
+
+        log.error(message, exception);
+
+        this.rollbar.error(exception, message);
+    }
+}
